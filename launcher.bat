2dd3a6e3d3787f97c53b4ddbfdd4d4c1f507f2b5
Camilo Yaya - Emo. <saenz292761@gmail.com>
2025-01-12 22:31:39 -0500

    add initial files

---
+++ launcher.bat
@@ -0,0 +1,37 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJEGF+VIgFBNASAuBL1e0A60J+9Tr+uiLq04hUuMpcYzU1PqHI+9z
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJhZks0
+::ZQ05rAF9IBncCkqN+0xwdVsFAlTi
+::ZQ05rAF9IAHYFVzEqQIEOhpQSReqPWm4Cfsf5+W7oqqzsEwTR+4+bIDJug==
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWE6T4wwkOhpQSReKPWm4Cfsf5+Wb
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATE8lA1LVtYRQeHMni/Rvhc5/j+4aqUrVgPV+df
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJEGF+VIgFBNASAuBL1euA6YIpebs096Vo0kYR+o+eo3UlLeBLaBeqnH2dJg+2ntOl9lCCQNdHg==
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+cd /d "%~dp0"  :: Cambia al directorio donde se encuentra el .bat
+start "" "nwjs-sdk\nw.exe" .  :: Ejecuta NW.js desde el directorio actual
+exit
